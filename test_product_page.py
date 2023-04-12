204a505ce12920b27cab1fcf543dce87d036d105
Stepan Prokopyev <stepkapro2002@gmail.com>
2023-04-12 14:43:09 +0300

    Refactored some code / put visit login page functional into BaseClass

--- test_product_page.py
+++ test_product_page.py
@@ -2,7 +2,7 @@ import pytest
 
 from .pages.product_page import ProductPage
 
-
+@pytest.mark.skip
 @pytest.mark.parametrize('link', [
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
@@ -47,3 +47,19 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.open()
     page.add_to_cart()
     page.should_disappear()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+
+    assert "/login" in browser.current_url, "it is not the Login Page"
